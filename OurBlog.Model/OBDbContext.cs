b44f355763e813caf9a214dcf2f2a98b468735ca
charles_xj <charles_xj@hotmail.com>
2016-11-29 16:50:04 +0800

    code first

---
+++ OurBlog.Model/OBDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace OurBlog.Model
+{
+    public partial class blogEntities : DbContext
+    {
+        //public blogEntities()
+        //     : base("name=blogEntities")
+        //{
+        //}
+
+        //public DbSet<discussinfos> discussinfos { get; set; }
+        //public DbSet<docclassts> docclassts { get; set; }
+        //public DbSet<docclsrelations> docclsrelations { get; set; }
+        //public DbSet<docclss> docclss { get; set; }
+        //public DbSet<dockeywords> dockeywords { get; set; }
+        //public DbSet<docs> docs { get; set; }
+        //public DbSet<docstatinfos> docstatinfos { get; set; }
+        //public DbSet<docstores> docstores { get; set; }
+        //public DbSet<docwritetimeorders> docwritetimeorders { get; set; }
+        //public DbSet<filemaps> filemaps { get; set; }
+        //public DbSet<menus> menus { get; set; }
+        //public DbSet<roleauthrights> roleauthrights { get; set; }
+        //public DbSet<roles> roles { get; set; }
+        //public DbSet<userroles> userroles { get; set; }
+        //public DbSet<users> users { get; set; }
+        //public DbSet<userselfsettings> userselfsettings { get; set; }
+        //public DbSet<userthirdnomaps> userthirdnomaps { get; set; }
+
+    }
+
+}
---
+++ OurBlog/Controllers/BaseController.cs
@@ -0,0 +1,76 @@
+﻿
+using OurBlog.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElecSmoke.Controllers
+{
+    public class BaseController : ExtendedController
+    {
+        public UserInfo UserInfo { set; get; }
+        #region 处理自动销毁
+        public IList<IDisposable> DisposableObjects { get; private set; }
+        public BaseController()
+        {
+            this.DisposableObjects = new List<IDisposable>();
+        }
+        protected void AddDisposableOject(object obj)
+        {
+            IDisposable disposable = obj as IDisposable;
+            if (null!=disposable)
+            {
+                this.DisposableObjects.Add(disposable);
+
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                foreach (IDisposable obj in this.DisposableObjects)
+                {
+                    if (null != obj)
+                    {
+                        obj.Dispose();
+                    }
+                }
+            }
+        }
+        #endregion
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //{
+            //    base.OnActionExecuting(filterContext);
+
+            //    var loginUsr = Session["loginUser"] as UserInfo;
+            //    string cookieSessionId = Request["sessionId"];
+            //    if (string.IsNullOrEmpty(cookieSessionId))
+            //    {
+            //        Response.Write("<script>window.top.location.href = '/BkLogin/Index'</script>");
+            //        Response.Flush();
+            //        Response.End();
+            //      //  Response.Redirect("BkLogin/Index");
+            //    }
+            //    var loginUsrJsonStr = CacheHelper.Get(cookieSessionId) as string;
+            //    if (string.IsNullOrEmpty(loginUsrJsonStr))
+            //    {
+            //        Response.Write("<script>window.top.location.href = '/BkLogin/Index'</script>");
+            //        Response.Flush();
+            //        Response.End();
+            //        //Response.Redirect("/BkLogin/Index");
+            //    }
+            //    else
+            //    {
+            //        UserInfo = SerializeHelper.DeSerializeStringToObject<UserInfo>(loginUsrJsonStr);
+            //        CacheHelper.set(cookieSessionId, loginUsrJsonStr,DateTime.Now.AddMinutes(20));
+
+            //    }
+        }
+
+
+    }
+}
